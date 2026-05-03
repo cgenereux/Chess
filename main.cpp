f3b58087dd43ee7b029cd78cd086670b003af1b6
cgenereux <curtisgenereux01@gmail.com>
2026-05-03 12:55:40 -0600

    feat: add left and right visual knight mirroring

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ void LoadPieceTextures() {
     pieceTextures[WP] = LoadTexture("res/white-pawn.png");
     pieceTextures[BP] = LoadTexture("res/black-pawn.png");
     pieceTextures[WN] = LoadTexture("res/white-knight-left.png");
-    pieceTextures[BN] = LoadTexture("res/black-knight-right.png");         
+    pieceTextures[BN] = LoadTexture("res/black-knight-left.png");         
     pieceTextures[WB] = LoadTexture("res/white-bishop.png");
     pieceTextures[BB] = LoadTexture("res/black-bishop.png");
     pieceTextures[WR] = LoadTexture("res/white-rook.png");
@@ -98,12 +98,39 @@ void drawPieces(Position &position) {
             Texture2D image = pieceTextures[pieceType];
 
             float scale = (float)tileSize / image.width;
-            DrawTextureEx(
-                image, // texture 
-                {(float)col*tileSize, (float)row*tileSize}, // position
-                float(0), // rotation (no rotation)
-                scale, // scale
-                WHITE // tint (white == no tint)
+
+            // default facing left
+            float flipMultiplier = -1.0f;
+            if (pieceType == WN || pieceType == BN) {
+                if (col >= 4) {
+                    // flip it to facing right
+                    flipMultiplier = 1.0f;
+                }
+            }
+
+            Rectangle sourceRectangle = { 
+                0.0f, 
+                0.0f, 
+                (float)image.width * flipMultiplier, 
+                (float)image.height 
+            };
+
+            Rectangle destRectangle = { 
+                (float)col * tileSize, 
+                (float)row * tileSize, 
+                (float)tileSize, 
+                (float)tileSize 
+            };
+
+            Vector2 origin = { 0.0f, 0.0f };
+
+            DrawTexturePro(
+                image, 
+                sourceRectangle, 
+                destRectangle, 
+                origin, 
+                0.0f,  // rotation (no rotation)
+                WHITE  // tint (white == no tint)
             );
         }
     }
